1de1ce849e7334d30b0e984b286646e45f139c0a
Azim <azimlord@gmail.com>
2021-05-30 23:03:16 +0800

    Fix atos issues

--- app/src/main/java/com/myfirst/buttonopennewactivity/MainActivity2.java
+++ app/src/main/java/com/myfirst/buttonopennewactivity/MainActivity2.java
@@ -1,10 +1,11 @@
 package com.myfirst.buttonopennewactivity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
 
